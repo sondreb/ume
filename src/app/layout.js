3029769f270e1110f76099a6b7de680144e45489
SondreB <pixelworld@hotmail.com>
2017-09-03 10:50:06 +0200

    Adds page navigation
    Adds page navigation.
    Adds layout to docs for prototype testing.

---
+++ src/app/layout.js
@@ -0,0 +1,34 @@
+
+
+function page(id) {
+
+    var pages = document.getElementsByClassName('page-open');
+
+    if (pages.length > 0) {
+        pages[0].classList.remove('page-open');
+    }
+
+    var page = document.getElementById('page-' + id);
+    page.classList.add('page-open');
+}
+
+function toggleMenu() {
+    document.getElementById('menu').classList.toggle('open');
+    document.getElementById('container').classList.toggle('open');
+    document.getElementById('content').classList.toggle('open');
+}
+
+(async function onStart() {
+
+    var actionLinks = document.querySelectorAll('[data-ume-page]');
+
+    actionLinks.forEach((action) => {
+        action.addEventListener('click', function () {
+            page(action.getAttribute('data-ume-page'));
+        });
+    });
+
+    document.getElementById('menu-button').addEventListener('click', toggleMenu);
+    document.getElementById('menu-button-close').addEventListener('click', toggleMenu);
+
+}());
