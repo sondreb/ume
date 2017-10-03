c3228c38424ab6be5f2aa21217f3a4ce6f908d0a
SondreB <pixelworld@hotmail.com>
2017-10-04 00:21:49 +0200

    Fixes settings a bit

--- src/app/v-app.js
+++ src/app/v-app.js
@@ -26,6 +26,29 @@ export class ume {
         console.log('onAlert was called on MyApp!');
     }
 
+    async onGatewaySave(v, event, source, data) {
+        var gateway = new Gateway();
+        gateway.name = data.gateway.name;
+        gateway.url = data.gateway.url;
+
+        await window.ume.storage.instance.put(gateway);
+
+        this.onSettings();
+    }
+
+    async onSettings() {
+        var gateways = await window.ume.storage.instance.getAll(Gateway.name);
+
+        console.log('GATEWAYS:', gateways);
+        var data = { gateways: gateways };
+
+        return data;
+    }
+
+    async removeGateway(v, event, source, data) {
+        console.log('Data should contain the gateway to remove: ', data);
+    }
+
     toggleMenu(v) {
         v.el('menu').classList.toggle('open');
         v.el('container').classList.toggle('open');
