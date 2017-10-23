39a5a57de0655610d4a517a60daa8b4cb08dd012
SondreB <pixelworld@hotmail.com>
2017-10-23 19:44:32 +0200

    Adds a proper docs page and moves app to dist folder

--- src/app/settings/home/home.component.ts
+++ src/app/settings/home/home.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+	public quota: string;
+	public usage: string;
+	private interval: any;
 
-  ngOnInit() {
-  }
+	constructor() { }
 
+	ngOnInit() {
+
+		// Calculate storage every 10 seconds.
+		this.interval = setInterval(() => {
+			this.updateUsedSpace();
+		}, 10000);
+
+		// Trigger an initial check.
+		setTimeout(() => {
+			this.updateUsedSpace();
+		});
+	}
+
+	private updateUsedSpace() {
+		const self = this;
+		const nav = (<any>window.navigator);
+
+		if (nav.storage) {
+			nav.storage.estimate().then((data) => {
+				self.quota = (data.quota / 1024 / 1024).toFixed(2);
+				self.usage = (data.usage / 1024 / 1024).toFixed(2);
+			});
+		}
+	}
 }
